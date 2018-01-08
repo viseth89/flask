842a7e83239f57ae43227ec3c8400e255f79c2b5
root <root@ubuntu-512mb-nyc3-01.localdomain>
2018-01-08 21:10:45 +0000

    finishing up video 7, understanding everything thats going

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def homepage():
     return render_template('main.html')
 
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+
 
 if __name__ == "__main__":
     app.run()
