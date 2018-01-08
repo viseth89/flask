da79fb5b952d86e2c0da8c4c879483e26de7b423
root <root@ubuntu-512mb-nyc3-01.localdomain>
2018-01-08 16:56:01 +0000

    end of video 3

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Hi there, how ya doin?"
+    return render_template('main.html')
 
 
 if __name__ == "__main__":
