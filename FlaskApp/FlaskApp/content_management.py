9359d524fa6d1ca39bfd74ecaa369efe8f18f146
root <root@ubuntu-512mb-nyc3-01.localdomain>
2018-01-09 00:43:46 +0000

    13:30 mark on video 8, digesting dictionary cms

---
+++ FlaskApp/FlaskApp/content_management.py
@@ -0,0 +1,7 @@
+
+def Content():
+    TOPIC_DICT = {"Basics":[['Introduction to Python','/introduction-to-python'],
+                            ['Print functions and Strings','/python-functions-and-strings']],
+                'Web Dev':[]}
+
+    return TOPIC_DICT
