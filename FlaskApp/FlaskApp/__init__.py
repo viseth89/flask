4438493b2cc177e54c82667996e32331f8048116
viseth89 <viseth89@gmail.com>
2018-01-18 11:29:29 -0600

    Finished Video 12 on local, Not yet pulled on server

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request, url_for, redirect
 from flask import Flask, render_template
 
 from content_management import Content
@@ -13,36 +13,64 @@ app = Flask(__name__)
 
 app.secret_key = 'my unobvious secret key'
 
+
 @app.route('/')
 def homepage():
     return render_template('main.html')
 
+
 @app.route('/dashboard/')
 def dashboard():
-    flash('flash test!!!')
-    flash('flash test!!!')
+
     flash('flash test!!!')
 
-    return render_template('dashboard.html', TOPIC_DICT = TOPIC_DICT)
+    return render_template('dashboard.html', TOPIC_DICT=TOPIC_DICT)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
 
+@app.errorhandler(405)
+def method_not_found(e):
+    return render_template('405.html')
 
-#Slash is only for testing
+@app.route('/about/')
+def about():
+    return (render_template('about.html'))
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login_page():
+    error = None
+    try:
+        if request.method == 'POST':
+            attempted_username=request.form['username']
+            attempted_password=request.form['password']
+
+            #flash(attempted_username)
+            #flash(attempted_password)
+
+            if attempted_username == 'admin' and attempted_password == 'password':
+                return redirect(url_for('dashboard'))
+            else:
+                error ='Invalid credentials. Try Again.'
+
+        return render_template('login.html', error=error)
+
+
+    except Exception as e:
+        #flash(e)
+        return render_template('login.html', error=error)
+
+# Slash is only for testing
 
 @app.route('/slashboard/')
 def slashboard():
     try:
-        return render_template('dashboard.html', TOPIC_DICT = shamwow)
+        return render_template('dashboard.html', TOPIC_DICT=shamwow)
     except Exception as e:
         return render_template('500.html', error=e)
 
 
-@app.route('/about')
-def about():
-	return (render_template('about.html')	
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
