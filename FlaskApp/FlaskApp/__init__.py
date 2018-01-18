740820304d869a0f9f617c55c9f84183bf2927ec
viseth89 <viseth89@gmail.com>
2018-01-18 09:41:12 -0600

    Finished Video 10: Flashing in Flask

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 
 from content_management import Content
 
@@ -6,18 +6,29 @@ TOPIC_DICT = Content()
 
 app = Flask(__name__)
 
+# kept gett this error
+# RuntimeError: The session is unavailable because no secret key was set.  Set the secret_key on the application to something unique and secret.
+# So I googled this answer for making app.secret_key
+
+app.secret_key = 'my unobvious secret key'
+
+
 @app.route('/')
 def homepage():
     return render_template('main.html')
 
 @app.route('/dashboard/')
 def dashboard():
+    flash('flash test!!!')
+    flash('flash test!!!')
+    flash('flash test!!!')
     return render_template('dashboard.html', TOPIC_DICT = TOPIC_DICT)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
 
+#Slash is only for testing
 @app.route('/slashboard/')
 def slashboard():
     try:
